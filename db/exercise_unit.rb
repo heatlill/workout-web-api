3a25a648e55e91aba41ffff95ac38423566529fa
Heath Lilley <heath.lilley@gmail.com>
2011-11-20 19:48:41 -0500

    Added support for rvm.

--- db/exercise_unit.rb
+++ db/exercise_unit.rb
@@ -5,7 +5,7 @@ require_relative 'database_helper'
 class ExerciseUnit < ActiveRecord::Base
     include DatabaseHelper
     set_table_name( 'exercise_unit' )
-    set_primary_key( 'id ')
+    set_primary_key( 'id')
 
     def to_s
         puts "#{id} #{name} #{description}"
