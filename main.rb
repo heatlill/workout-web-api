35cc0ef1ca88fa36d0cfaf1f76dbb1211b98cc3a
Heath Lilley <heath.lilley@gmail.com>
2012-04-22 09:45:22 -0400

    Made basic modifications to main.rb and exercise.haml.  Fixed jquery path.

--- main.rb
+++ main.rb
@@ -1,5 +1,5 @@
-##!/usr/bin/ruby
 #!/home/heath/.rvm/rubies/ruby-1.9.2-p180/bin/ruby
+# -- #!/usr/bin/ruby
 
 require 'rubygems'
 require 'sinatra'
@@ -8,19 +8,8 @@ require 'haml'
 require_relative 'db/database_helper'
 require_relative 'db/exercise'
 
-#ActiveRecord::Base.establish_connection(
-#  :adapter => "mysql",
-#  :host => "localhost",
-#  #:username => "<your database username>",
-#  #:password => "<your database password>",
-#  :database => "test"
-#)
 include DatabaseHelper
 use Rack::MethodOverride
-
-get '/' do
-    'Hello World!'	
-end
 
 get '/workout_api/show_exercises/' do
 	exercises = Exercise.find( :all )
