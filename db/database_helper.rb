800c73c4214785675633944ee7b3de8eb32ac539
Heath Lilley <heath.lilley@gmail.com>
2012-04-29 17:47:00 -0400

    Made database_helper.rb more heroku friendly.

--- db/database_helper.rb
+++ db/database_helper.rb
@@ -1,15 +1,17 @@
 require 'rubygems'
 require 'active_record'
-	
+require 'uri'
+
 module DatabaseHelper
-    database_url = ENV['database_url'] 
-    database_url |= 'test'
-    DB_PARAMS = {
-        :adapter  => "postgresql",
-        :host     => "localhost",
-        :database => "test",
-        :username => "postgres",
-        :password => "test"
-    }
-	ActiveRecord::Base.establish_connection(DB_PARAMS)
+    #DB_PARAMS = {:adapter => "postgresql",:host => "localhost",:database => "test",:username => "postgres",:password => "test"}
+    
+    db = URI.parse(ENV['DATABASE_URL'] || 'postgresql://localhost/test')
+    ActiveRecord::Base.establish_connection(
+        :adapter => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
+        :host => db.host,
+        :username => db.user.nil? ? 'postgres' : db.user,
+        :password => db.password.nil? ? 'test' : db.password,
+        :database => db.path[1..-1],
+        :encoding => 'utf8'
+    )
 end
