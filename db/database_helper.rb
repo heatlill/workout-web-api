d50e0b60690e46efdde93f9023d5c7bc8ffee72e
Heath Lilley <lilleyh@Heaths-MacBook-Air.local>
2012-06-20 12:53:02 -0400

    Restructured schema setup/maintenance files.

--- db/database_helper.rb
+++ db/database_helper.rb
@@ -3,14 +3,14 @@ require 'active_record'
 require 'uri'
 
 module DatabaseHelper
-    #DB_PARAMS = {:adapter => "postgresql",:host => "localhost",:database => "test",:username => "postgres",:password => "test"}
+    #DB_PARAMS = {:adapter => "postgresql",:host => "localhost",:database => "test",:username => "postgres",:password => "bruiser"}
     
     db = URI.parse(ENV['DATABASE_URL'] || 'postgresql://localhost/test')
     DB_PARAMS = {    
         :adapter => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
         :host => db.host,
         :username => db.user.nil? ? 'postgres' : db.user,
-        :password => db.password.nil? ? 'test' : db.password,
+        :password => db.password.nil? ? 'bruiser' : db.password,
         :database => db.path[1..-1],
         :encoding => 'utf8'
     }
