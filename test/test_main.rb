5dab27cd4fa07a91361bd34b76eed0889c9334c6
lilleyh <heath.lilley@gmail.com>
2012-10-06 12:15:22 -0400

    Disabled test_main.rb.

--- test/test_main.rb
+++ test/test_main.rb
@@ -1,21 +1,23 @@
-#/usr/bin/ruby
 require 'rubygems'
 require 'test/unit'
 require 'rack/test'
 require_relative '../main'
 
-#set :environment, :test
 ENV['RACK_ENV'] = 'test'
 
 class TestWorkout < Test::Unit::TestCase
-    include  Rack::Test::Methods
-
+    include  Rack::Test::Methods 
     def app
         Sinatra::Application
     end
 
     def test_save_workout
         value = '[{"test":"value"}]'
+        #value = '[{"workout":
+        #            {"session_exercises":[
+        #                    {"id":"123","exercise_id":"1","repitition_count":"5","sequence_number":"1"},
+        #            ]
+        #        }]'
         begin
             last_response = post 'http://localhost/workout_api/save_workout', params={:workout => value}
         rescue Error => error
